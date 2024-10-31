2b5b0bbf93c7ab2b41a1e89f418e3756718d6648
fedro <151860420+xfdrxndre@users.noreply.github.com>
2024-10-31 14:47:34 +0700

    mucachips.js

--- mucachips.js
+++ mucachips.js
@@ -139,16 +139,16 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     const hamburger = document.querySelector(".hamburger");
-    const navMenu = document.querySelector(".nav-menu");
+    const navMenuHamburger = document.querySelector(".nav-menu");
 
     hamburger.addEventListener("click", () => {
         hamburger.classList.toggle("active");
-        navMenu.classList.toggle("active");
+        navMenuHamburger.classList.toggle("active");
         console.log("Hamburger clicked!"); // Untuk debugging
     });
 
     document.querySelectorAll(".nav-menu a").forEach(n => n.addEventListener("click", () => {
         hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
+        navMenuHamburger.classList.remove("active");
     }));
 });
