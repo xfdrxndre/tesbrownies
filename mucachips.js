4bc623c22313b78d621d447c87b0c5d3eba024c3
fedro <151860420+xfdrxndre@users.noreply.github.com>
2024-10-31 11:54:47 +0700

    mucachips.js

--- mucachips.js
+++ mucachips.js
@@ -144,3 +144,21 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hamburger.addEventListener('click', function() {
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+
+    // Menutup menu saat link diklik
+    document.querySelectorAll('.nav-menu a').forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            navMenu.classList.remove('active');
+        });
+    });
+});
